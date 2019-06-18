c36c052bfc0fb7b44fe1929eb7fc528105fa8e14
John Fajardo <fajardocj@gmail.com>
2019-06-18 16:54:02 -0400

    Done

--- looping.rb
+++ looping.rb
@@ -1,5 +1,13 @@
 def looping
-	#your code here
+	counter = 1
+	loop do
+		puts "Never pass a school bus if it has flashing red lights"
+		counter = counter + 1
+		if counter > 10
+			break
+		end
+	end
 end
 
 #call your method here
+looping
